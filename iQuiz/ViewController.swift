b3fc6b21475c8bba138a8dadba2c7b991e170a14
Kira Brodsky <kbrods@uw.edu>
2025-04-29 14:40:25 -0700

    Initial Commit

---
+++ iQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by Kira Brodsky on 4/29/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iQuizTests/iQuizTests.swift
@@ -0,0 +1,17 @@
+//
+//  iQuizTests.swift
+//  iQuizTests
+//
+//  Created by Kira Brodsky on 4/29/25.
+//
+
+import Testing
+@testable import iQuiz
+
+struct iQuizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
