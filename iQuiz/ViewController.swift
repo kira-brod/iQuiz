b3b6cb71ff2c79fad5bd70ac4de70be4172ee6f9
Kira Brodsky <kbrods@uw.edu>
2025-05-03 13:57:08 -0700

    iquiz p1

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -7,13 +7,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
 
+    @IBOutlet weak var tblTable: UITableView!
+    
+//    let stringTableData = StringTableDataModel([
+//        "Mathematics",
+//        "Marvel Super Heroes",
+//        "Science"
+//    ])
+    
+    class StringTableData: NSObject, UITableViewDataSource {
+        
+        let data : [String] = [
+            "Mathematics",
+            "Marvel Super Heroes",
+            "Science"
+        ]
+        
+        let subtitles : [String] = [
+            "Take a quiz about mathematics",
+            "Take a quiz about Marvel Super Heroes",
+            "Take a quiz about Science"
+        ]
+        
+//        init(_ items : [String]) {
+//            data = items
+//        }
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return data.count
+        }
+
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "StringCell")!
+            cell.textLabel?.text = data[indexPath.row]
+            cell.detailTextLabel?.text = subtitles[indexPath.row]
+            return cell
+        }
+    }
+    
+    let stringTableData = StringTableData()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tblTable.dataSource = stringTableData
+        tblTable.delegate = self
     }
-
+    
+    @IBAction func Settings(_ sender: Any) {
+        let alert = UIAlertController(title: "Settings", message: "Settings go here", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Close the alert"),style: .default, handler: { _ in
+              alert.dismiss(animated: true)
+        }))
+        self.present(alert, animated: true)
+    }
+                                                
 
 }
-
+                        
+                                                                                                                                                                                                                                                        
